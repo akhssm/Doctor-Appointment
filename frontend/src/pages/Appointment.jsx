5f5b1fff793afac73db14aaca199e188fa7e17ec
Akshay <mandaakshay20@gmail.com>
2026-02-13 11:52:04 +0530

    created book and appointment button

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -128,6 +128,7 @@ const Appointment = () => {
             </p>
           ))}
         </div>
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
       </div>
     </div>
   )
