8f4b45d0964ed579788a1cfd050ad34eac469df0
Akshay <mandaakshay20@gmail.com>
2026-02-25 12:16:29 +0530

    We can login and stay in hone page

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,11 +8,9 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
     const backendurl = import.meta.env.VITE_BACKEND_URL
-    const [doctors, setDoctors] = useState([])
 
-    const value = {
-        doctors, currencySymbol
-    }
+    const [doctors, setDoctors] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
 
     const getDoctorsData = async () => {
         try {
@@ -27,6 +25,13 @@ const AppContextProvider = (props) => {
             console.log(error)
             toast.error(error.message)
         }
+    }
+
+    const value = {
+        doctors, 
+        currencySymbol, 
+        token, setToken,
+        backendurl
     }
 
     useEffect(() => {
