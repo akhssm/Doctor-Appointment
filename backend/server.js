bd9ea8db4d70ac108c558afd62eb9f5782c4ba8a
Akshay <mandaakshay20@gmail.com>
2026-02-25 11:07:11 +0530

    We can login as user successfully

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 
 // app config
@@ -24,7 +25,8 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/admin", adminRouter);
-app.use("/api/doctor", doctorRouter)
+app.use("/api/doctor", doctorRouter);
+app.use("/api/user", userRouter)
 
 app.listen(Port, () => {
   console.log(`Server running on port ${Port}`);
