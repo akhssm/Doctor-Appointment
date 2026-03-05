8cdbe60536e53b654cef8db103017b6fbfbd3551
Akshay <mandaakshay20@gmail.com>
2026-03-05 11:30:18 +0530

    We can display all appointments in admin page

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -4,8 +4,26 @@ export const AppContext = createContext()
 
 const AppContextProvider = ({ children }) => {
 
-    const value = {
+    const currency = '$'
 
+    const calculateAge = (dob) => {
+        const today = new Date()
+        const birthDate = new Date(dob)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        return age
+    }
+
+    const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+  // Format date
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split('_')
+    return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+  }
+
+    const value = {
+        calculateAge, slotDateFormat, currency
     }
 
     return (
