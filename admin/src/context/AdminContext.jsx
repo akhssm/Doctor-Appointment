6caad47cefbec8c4ca229607f8057de103f29b43
Akshay <mandaakshay20@gmail.com>
2026-03-05 15:34:52 +0530

    We have completed admin dashboard

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ const AdminContextProvider = ({ children }) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
     const [doctors, setDoctors] = useState([])
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -87,6 +88,25 @@ const AdminContextProvider = ({ children }) => {
         }
 
     }
+
+    const getDashData = async () => {
+
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/admin/dashboard', {headers:{aToken}})
+
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
  
     const value = {
         aToken, setAToken, 
@@ -94,7 +114,8 @@ const AdminContextProvider = ({ children }) => {
         getAllDoctors, changeAvailability,
         appointments, setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData, getDashData
     }
 
     return (
