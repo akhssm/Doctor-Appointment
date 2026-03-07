1fb9ecad50805c3d03e3d761280b5aced066d2d7
Akshay <mandaakshay20@gmail.com>
2026-03-07 12:26:41 +0530

    Completed dashboard and appointments page of doctor

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -11,6 +11,7 @@ const DoctorContextProvider = ({ children }) => {
 
     const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const getAppointments = async () => {
         try {
@@ -69,13 +70,31 @@ const DoctorContextProvider = ({ children }) => {
 
     }
 
+    const getDashData = async () => {
+
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/doctor/dashboard', {headers: {dToken}})
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
 
     const value = {
         dToken, setDToken,
         backendUrl,
         appointments, setAppointments,
         getAppointments,
-        completeAppointment, cancelAppointment
+        completeAppointment, cancelAppointment,
+        dashData, setDashData, getDashData
     }
 
     return (
